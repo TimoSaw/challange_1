db06cce101e5619d1a8155733b57e769b964f14b
TimoSaw <timosawitzky@gmail.com>
2023-12-10 14:35:15 +0100

    update

--- main.js
+++ main.js
@@ -1,177 +1,78 @@
-//IMPORT MODULES
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import GUI from 'lil-gui';
 
-//CONSTANT & VARIABLES
-let width = window.innerWidth;
-let height = window.innerHeight;
-//-- GUI PARAMETERS
-var gui;
-const parameters = {
-  resolutionX: 3,
-  rotationX: 100
+//VARIABLES
+let width = window.innerWidth
+let height = window.innerHeight
+
+
+//CREAT SCENE AND CAMERA 
+const scene = new THREE.Scene();
+const camera = new  THREE.PerspectiveCamera(45, width / height, 0.1, 100)
+camera.position.set(0, 0, 20);
+
+//CREATE A GEOMETRY
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0xeb3434 } );
+
+
+const cube = new THREE.Mesh( geometry, material );
+
+var mx = -5; 
+const myArray = [];
+
+for (var i = 0;i< 10; i++) {
+  var mesh = new THREE.Mesh( geometry, material );
+  mesh.position.x = mx;
+  scene.add( mesh );
+  myArray.push(mesh)
+  mx+= 1.1;
 }
 
-//-- SCENE VARIABLES
-var scene;
-var camera;
-var renderer;
-var container;
-var control;
-var ambientLight;
-var directionalLight;
 
-//-- GEOMETRY PARAMETERS
-//Create an empty array for storing all the cubes
-let sceneCubes = [];
-let resX = parameters.resolutionX;
-let rotX = parameters.rotationX;
+// scene.add( cube );
 
-function main(){
-  //GUI
-  gui = new GUI;
-  gui.add(parameters, 'resolutionX', 1, 10, 1);
-  gui.add(parameters, 'rotationX', 0, 180);
-
-  //CREATE SCENE AND CAMERA
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 15, width / height, 0.1, 100);
-  camera.position.set(10, 10, 10)
-
-  //LIGHTINGS
-  ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-  scene.add(ambientLight);
-
-  directionalLight = new THREE.DirectionalLight( 0xffffff, 1);
-  directionalLight.position.set(2,5,5);
-  directionalLight.target.position.set(-1,-1,0);
-  scene.add( directionalLight );
-  scene.add(directionalLight.target);
-
-  //GEOMETRY INITIATION
-  // Initiate first cubes
-  createCubes();
-  rotateCubes();
-
-  //RESPONSIVE WINDOW
-  window.addEventListener('resize', handleResize);
- 
-  //CREATE A RENDERER
-  renderer = new THREE.WebGLRenderer({alpha:true, antialias:true});
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-  container = document.querySelector('#threejs-container');
-  container.append(renderer.domElement);
-  
-  //CREATE MOUSE CONTROL
-  control = new OrbitControls( camera, renderer.domElement );
-
-  //EXECUTE THE UPDATE
-  animate();
-}
- 
-//-----------------------------------------------------------------------------------
-//HELPER FUNCTIONS
-//-----------------------------------------------------------------------------------
-//GEOMETRY FUNCTIONS
-// Create Cubes
-function createCubes(){
-  for(let i=0; i<resX; i++){
-    const geometry = new THREE.BoxGeometry(0.1, 1,1);
-    const material = new THREE.MeshPhysicalMaterial();
-    material.color = new THREE.Color(0xffffff);
-    material.color.setRGB(0,0,Math.random());
-
-    const cube = new THREE.Mesh(geometry, material);
-    cube.position.set(i*0.1, 0, 0);
-    cube.name = "cube " + i;
-    sceneCubes.push(cube);
-
-    scene.add(cube);
-  }
-}
-
-//Rotate Cubes
-function rotateCubes(){
-  sceneCubes.forEach((element, index)=>{
-    let scene_cube = scene.getObjectByName(element.name);
-    let radian_rot = (index*(rotX/resX)) * (Math.PI/180);
-    scene_cube.rotation.set(radian_rot, 0, 0)
-    rotX = parameters.rotationX;
-  })
-}
-
-//Remove 3D Objects and clean the caches
-function removeObject(sceneObject){
-  if (!(sceneObject instanceof THREE.Object3D)) return;
-
-  //Remove the geometry to free GPU resources
-  if(sceneObject.geometry) sceneObject.geometry.dispose();
-
-  //Remove the material to free GPU resources
-  if(sceneObject.material){
-    if (sceneObject.material instanceof Array) {
-      sceneObject.material.forEach(material => material.dispose());
-    } else {
-        sceneObject.material.dispose();
-    }
-  }
-
-  //Remove object from scene
-  sceneObject.removeFromParent();
-}
-
-//Remove the cubes
-function removeCubes(){
-  resX = parameters.resolutionX;
-  rotX = parameters.rotationX;
-
-  sceneCubes.forEach(element =>{
-    let scene_cube = scene.getObjectByName(element.name);
-    removeObject(scene_cube);
-  })
-
-  sceneCubes = [];
-}
-
-//RESPONSIVE
 function handleResize() {
-  width = window.innerWidth;
-  height = window.innerHeight;
-  camera.aspect = width / height;
-  camera.updateProjectionMatrix();
-  renderer.setSize(width, height);
-  renderer.render(scene, camera);
-}
+    width = window.innerWidth;
+    height = window.innerHeight;
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(width, height);
+    renderer.render(scene, camera);
+  }
+  
+  
+  window.addEventListener('resize', handleResize);
 
+//CREATE A RENDERER
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+const container = document.querySelector('#threejs-container');
+container.append(renderer.domElement);
+
+//CREATE MOUSE CONTROL
+const controls = new OrbitControls( camera, renderer.domElement );
 
 //ANIMATE AND RENDER
+
+
+
 function animate() {
-	requestAnimationFrame( animate );
- 
-  control.update();
+    requestAnimationFrame( animate );
+    
+    for (let i = 0;i < myArray.length; i++) {
+    myArray[i].rotation.z += 0.01 * Math.random();
+    myArray[i].rotation.y += 0.01 * Math.random();
+    }
 
-  if(resX != parameters.resolutionX){
-    removeCubes();
-    createCubes();
-    rotateCubes();
+    renderer.render( scene, camera );
   }
+  
+  
+  animate();
+  
 
-  if (rotX != parameters.rotationX){
-    rotateCubes();
-  }
  
-	renderer.render( scene, camera );
-}
-//-----------------------------------------------------------------------------------
-// CLASS
-//-----------------------------------------------------------------------------------
-
-
-//-----------------------------------------------------------------------------------
-// EXECUTE MAIN 
-//-----------------------------------------------------------------------------------
-
-main();
